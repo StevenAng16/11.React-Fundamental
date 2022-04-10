2be93dc3d3bfcd9211a8d6b1f61bfaacb6656497
Steven <angsteven7@gmail.com>
2022-04-10 22:05:49 +0700

    Screenshot and summary.md

---
+++ my-react-app/src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <>
+        <span><p style={{border: "1px solid black"}}><s>Membuat Komponen</s></p></span>
+        <p>Unit Testing</p>
+        <p><s>Setup Development Enviroment</s></p>
+        <p>Deploy ke server</p>
+      </>
+      
+    );
+  };
+  
+  export default Home;
